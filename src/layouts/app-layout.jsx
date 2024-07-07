14f1247346e35384dbc9c2fced04b3ba92d39822
Yashwardhan Tulsyan <yashrajtulsyan@gmail.com>
2024-07-07 22:35:37 +0530

    Create Landing, Dashboard & Login page

---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,19 @@
+import Header from "@/components/header"
+import { Outlet } from "react-router-dom"
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className="container min-h-screen">
+        <Header/>
+        <Outlet/>
+      </main>
+
+      <div className="p-10 mt-10 text-center bg-gray-800">
+        Made with 💖 by Yashwardhan Tulsyan
+      </div>
+    </div>
+  )
+}
+
+export default AppLayout
